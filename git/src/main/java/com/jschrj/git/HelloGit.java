a5ef5724b36c353e7376413196bbf6e3e89da57b
xtldn_glb <306206438@qq.com>
2020-11-21 11:02:13 +0800

    my second git idea

--- git/src/main/java/com/jschrj/git/HelloGit.java
+++ git/src/main/java/com/jschrj/git/HelloGit.java
@@ -3,5 +3,6 @@ package com.jschrj.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("更新1");
     }
 }
